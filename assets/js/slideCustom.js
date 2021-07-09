bd3df728efdbcf96c674c8dfd3dabcb88d04a28e
gitname <82920762+dsa02070@users.noreply.github.com>
2021-07-09 23:41:20 +0900

    2021.07.09. 슬라이드 추가
    1. 총 3가지 슬라이드 완성

--- assets/js/slideCustom.js
+++ assets/js/slideCustom.js
@@ -1,6 +1,38 @@
 $(function () {
 
+    var swiper01 = new Swiper(".main_banner01", {
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+        autoplay: {
+            delay: 3000,
+        },
+    });
 
+    var swiper02 = new Swiper(".main_banner02", {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000,
+        },
+    });
 
-
+    var swiper03 = new Swiper(".section_slide01", {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 15,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000,
+        },
+    });
 }) // document ready
